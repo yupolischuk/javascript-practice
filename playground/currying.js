8599c7c9b567895c2e6e67f977d45a1d93ded9e8
yupolischuk <yumuscletank@gmail.com>
2019-02-03 00:20:07 +0200

    Added to currying

--- playground/currying.js
+++ playground/currying.js
@@ -1,4 +1,46 @@
-function myCarry() {
+/*
+Apply any function to defined number of arguments
+*/
+function curry(fn) {
+  function nest(N, args) {
+    return (...xs) => {
+      if (N - xs.length <= 0) {
+        return fn(...args, ...xs);
+      }
+      return nest(N - xs.length, [...args, ...xs]);
+    };
+  }
+  return nest(fn.length, []);
+}
+
+/*
+// Another variant
+function curry(fn) {
+  return (...xs) => {
+    if (xs.length === 0) {
+      throw Error('EMPTY INVOCATION');
+    }
+    if (xs.length >= fn.length) {
+      return fn(...xs);
+    }
+    return curry(fn.bind(null, ...xs));
+  };
+}
+*/
+const sum3 = curry((x, y, z) => x + y + z);
+
+console.log('*** Apply any function to defined number of arguments');
+console.log(
+  sum3(5, 10, 15),
+  sum3(5, 10)(15),
+  sum3(5)(10, 15),
+  sum3(5)(10)(15),
+);
+
+/*
+ Collect any number of params to array
+*/
+function carryCollectToArray() {
   var ap = Array.prototype;
   var args = arguments;
 
@@ -17,8 +59,8 @@ function myCarry() {
   }
 }
 
-var f = myCarry();
+var f = carryCollectToArray();
 var a = f(5)(10)(15);
+console.log('*** Collect any number of params to array');
 a();
-
 //(myCarry()(2)(5)(10))();
