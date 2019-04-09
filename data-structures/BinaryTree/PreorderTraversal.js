5db504718e81e4b6ecc71deb356b7b05c682bd1a
yupolischuk <yumuscletank@gmail.com>
2019-04-09 14:11:42 +0300

    Added binary tree iterative preorder traversal

--- data-structures/BinaryTree/PreorderTraversal.js
+++ data-structures/BinaryTree/PreorderTraversal.js
@@ -22,6 +22,11 @@ var TreeNode = {
 };
 
 
+// var TreeNode =  {
+//   val: 3,
+//   right: TreeNode = { val: 2, right: null, left: null },
+//   left: TreeNode = { val: 1, right: null, left: null } };
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -29,23 +34,41 @@ var TreeNode = {
  *     this.left = this.right = null;
  * }
  */
+
 /**
+ * Recursive sollution
  * @param {TreeNode} root
  * @return {number[]}
  */
-// Recursive sollution
-var preorderTraversal = function(root, arr = []) {
-    //console.log(root.right.val);
-    //console.log(root);
+var preorderTraversalRecursive = function(root, arr = []) {
     if (root) {
       arr.push(root.val);
-      preorderTraversal(root.right, arr);
-      preorderTraversal(root.left, arr);
+      preorderTraversalRecursive(root.left, arr);
+      preorderTraversalRecursive(root.right, arr);
     }
   
     return arr;
 };
 
-console.log(preorderTraversal(TreeNode));
+/**
+ * Iterative sollution
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversalIterative = function(root) {
+  if (!root) { return [] };
+  var res = [];
+  var stack = [root];
+
+  while(stack.length) {
+    var node = stack.pop();
+    res.push(node.val);
+    if (node.right) { stack.push(node.right) };
+    if (node.left) { stack.push(node.left) };
+  }
+  return res;
+}
 
 
+console.log(preorderTraversalRecursive(TreeNode));
+console.log(preorderTraversalIterative(TreeNode));
