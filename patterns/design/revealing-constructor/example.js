475cea589a531a7e0ff359401dd536b844b3e0e3
yupolishchuk <yumuscletank@gmail.com>
2019-10-27 14:14:26 +0200

    Refactor structure

--- patterns/design/revealing-constructor/example.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const ticker = require('./ticker');
-
-ticker.on('tick', (tickCount) => console.log(tickCount, 'TICK'));
-// ticker.emit('something', {}); <-- This will fail (because read onle event emitter)
-// require('events').prototype.emit.call(ticker, 'someEvent', {}); <-- This workaround will instead work
