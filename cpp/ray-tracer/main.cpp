ad7d19a105baf04ea59c49aa53031b7fb6ad9e98
yupolishchuk <yumuscletank@gmail.com>
2020-01-22 18:24:29 +0200

    add diffuse materials

--- cpp/ray-tracer/main.cpp
+++ cpp/ray-tracer/main.cpp
@@ -6,10 +6,19 @@
 
 #include <iostream>
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0*vec3(random_double(), random_double(), random_double()) - vec3(1,1,1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hittable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
@@ -40,7 +49,7 @@ int main() {
                 col += color(r, world);
             }
             col /= float(ns);
-
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
