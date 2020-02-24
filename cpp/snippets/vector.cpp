b78092c0d6d7a3f33074fb45d22c8d1e20bb9771
yupolishchuk <yumuscletank@gmail.com>
2020-02-24 18:31:29 +0200

    add sorting vectors

--- cpp/snippets/vector.cpp
+++ cpp/snippets/vector.cpp
@@ -11,11 +11,19 @@ void print_vector(vector<auto> vect)
         cout << i << ' ';
 }
 
+void sort_vector(vector<auto>& vect)
+{
+    sort(vect.begin(), vect.end()); // asc
+//    sort(vect.begin(), vect.end(), greater<int>()); // desc, for ints, chars
+//    sort(vect.begin(), vect.end(), greater<string>()); // desc, for strings
+}
+
 int main()
 {
     // initialize
     vector<int> vect{ 40, 10, 20, 30 };
-//    vector<char> chars{ 'a', 'b', 'c' };
+//    vector<char> vect{ 'a', 'b', 'c' };
+//    vector<string> vect {"ab", "bc","bb", "cc"};
 
     // print
     print_vector(vect);
@@ -23,7 +31,7 @@ int main()
     cout << endl << "*****************" << endl;
 
     // sort
-    sort(vect.begin(), vect.end());
+    sort_vector(vect);
 
     print_vector(vect);
 }
