b336bd670f936eb4153d421b7502129991895403
yupolishchuk <yumuscletank@gmail.com>
2020-03-20 14:59:29 +0200

    add binary tree example

---
+++ computer-science/data-structures/tree/BinaryTree.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstddef>
+
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* insert(Node* root, int data) {
+        if(root == NULL) {
+            return new Node(data);
+        } else {
+            Node* cur;
+            if(data <= root->data) {
+                cur = insert(root->left, data);
+                root->left = cur;
+            } else {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+
+            return root;
+        }
+    }
+
+
+    void preOrder(Node *root) {
+        if (root == NULL) { return; }
+
+        std::cout << root->data << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+
+    void postOrder(Node *root) {
+        if (root == NULL) { return; }
+        postOrder(root->left);
+        postOrder(root->right);
+        std::cout << root->data << " ";
+    }
+
+    void inOrder(Node *root) {
+        if (root == NULL) { return; }
+        inOrder(root->left);
+        std::cout << root->data << " ";
+        inOrder(node->right);
+    }
+
+
+}; //End of Solution
+
+/*
+ Input
+ 6
+ 1 2 5 3 6 4
+
+  1
+  \
+   2
+    \
+     5
+    /  \
+   3    6
+    \
+     4
+
+ Output
+ Preorder
+ 1 2 5 3 4 6
+
+ Postorder
+ 4 3 6 5 2 1
+
+ Inorder
+ 1 2 3 4 5 6 
+
+*/
+int main() {
+
+    Solution myTree;
+    Node* root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    myTree.preOrder(root);
+
+    return 0;
+}
--- computer-science/data-structures/tree/BinaryTree2.cpp
+++ computer-science/data-structures/tree/BinaryTree2.cpp
@@ -68,9 +68,9 @@ void printPreorder(struct Node* node)
 int main()
 {
     struct Node *root = new Node(1);
-    root->left             = new Node(2);
-    root->right         = new Node(3);
-    root->left->left     = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
     root->left->right = new Node(5);
 
     cout << "\nPreorder traversal of binary tree is \n";
